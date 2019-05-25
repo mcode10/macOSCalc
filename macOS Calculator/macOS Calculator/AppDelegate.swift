eac23034f77b1259bba8d5d2a2b8c5977c13ddb8
Manas Paranjape <manas@designX6.com>
2019-05-25 18:40:20 -0400

    This is the intial commit.
    * I have tweaked the menus and will eventually incorporate them.
    * I imported CalcEngine and CalcEngineProtocol, since there's no need to
    make new ones.

---
+++ macOS Calculator/macOS Calculator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macOS Calculator
+//
+//  Created by Manas Paranjape on 5/25/19.
+//  Copyright © 2019 DesignX6. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macOS Calculator/macOS Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOS Calculator
+//
+//  Created by Manas Paranjape on 5/25/19.
+//  Copyright © 2019 DesignX6. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
